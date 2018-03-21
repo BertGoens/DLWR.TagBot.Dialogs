884f4e443adedf1e3e2504e75d2479af583b4fca
Goens, Bert <goensb@delawareconsulting.com>
2018-03-21 10:54:19 +0100

    add emoji for better user retention

--- src/dialogs/greeting.ts
+++ src/dialogs/greeting.ts
@@ -3,7 +3,14 @@ import * as builder from "botbuilder";
 export const GreetingLuisName = "Utilities.Greeting";
 export const GreetingDialog: builder.IDialogWaterfallStep[] = [
   function sendHelpMessage(session, args, next) {
-    var message = new builder.Message().text("Hi, let's tag some documents!");
+    var message = new builder.Message()
+      .text("Hi, let's tag some documents!")
+      .suggestedActions(
+        builder.SuggestedActions.create(session, [
+          builder.CardAction.imBack(session, "Search", "👍"),
+          builder.CardAction.imBack(session, "Quit", "👎")
+        ])
+      );
     session.send(message);
   }
 ];
