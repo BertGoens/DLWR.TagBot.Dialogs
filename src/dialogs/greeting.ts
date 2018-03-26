7d1880558121237ccb074fed94ee2124278c0073
Goens, Bert <goensb@delawareconsulting.com>
2018-03-26 17:09:20 +0200

    encapsulate greetingMessage

--- src/dialogs/greeting.ts
+++ src/dialogs/greeting.ts
@@ -1,16 +1,20 @@
 import * as builder from "botbuilder";
 
 export const GreetingLuisName = "Utilities.Greeting";
+
+export const greetingMessage = session => {
+  return new builder.Message()
+    .text("Hi, let's tag some documents!")
+    .suggestedActions(
+      builder.SuggestedActions.create(session, [
+        builder.CardAction.imBack(session, "Search", "👍"),
+        builder.CardAction.imBack(session, "Quit", "👎")
+      ])
+    );
+};
+
 export const GreetingDialog: builder.IDialogWaterfallStep[] = [
   function sendHelpMessage(session, args, next) {
-    var message = new builder.Message()
-      .text("Hi, let's tag some documents!")
-      .suggestedActions(
-        builder.SuggestedActions.create(session, [
-          builder.CardAction.imBack(session, "Search", "👍"),
-          builder.CardAction.imBack(session, "Quit", "👎")
-        ])
-      );
-    session.send(message);
+    var message = session.send(greetingMessage(session));
   }
 ];
