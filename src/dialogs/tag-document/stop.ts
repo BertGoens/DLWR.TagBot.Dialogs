04372df7de28d1be7112578447f9fceae816a2bf
Goens, Bert <goensb@delawareconsulting.com>
2018-04-24 14:37:54 +0200

    send message when quitting

--- src/dialogs/tag-document/stop.ts
+++ src/dialogs/tag-document/stop.ts
@@ -3,6 +3,6 @@ import * as builder from 'botbuilder'
 export const StopIntent: builder.IDialogWaterfallStep[] = [
 	function quitConversaino(session, args, next) {
 		const msg = new builder.Message().text("Alright, I didn't add any tags")
-		session.endDialog()
+		session.endDialog(msg)
 	},
 ]
